374586b62f3f27bdabd8da9a03503761f9dd83bb
fali14 <ffatimaali13@gmail.com>
2019-06-07 14:41:46 -0400

    original inputs

--- test_git.py
+++ test_git.py
@@ -2,16 +2,8 @@ SITES = [
    "https://test1.com",   "https://test2.com",
    "https://test3.com",
    "https://test4.com",
-   "https://test5.com",
+   "https://test5.com", "https://test6.com",
    "fatima",
    "https://test7.com"
 ]
 
-SITES = [
-   "https://test1.com",   "https://test2.com",
-   "https://test3.com",
-   "https://test4.com",
-   "https://test5.com",
-   "fatima",
-   "https://test7.com"
-]
