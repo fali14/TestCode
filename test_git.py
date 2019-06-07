2b77b69eb0afa35cafa50b3b8b55dbf1c2fa90ce
fali14 <ffatimaali13@gmail.com>
2019-06-07 10:03:27 -0400

    updated by adding 5 names

---
+++ test_git.py
@@ -0,0 +1,15 @@
+SITES = [
+   "https://test1.com",
+   "https://test2.com",
+   "https://test3.com",
+   "https://test4.com",
+   "https://test5.com",
+   "Fatima Ali",
+   "https://test6.com",
+   "Fatima Ali",
+   "Fatima Ali",
+   "Fatima Ali",
+   "Fatima Ali",   
+   "Fatima Ali",
+   "https://test7.com"
+]
