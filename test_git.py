9e0f745e565cd2b83e52cabf138befe5917f0c56
fali14 <ffatimaali13@gmail.com>
2019-06-07 10:14:25 -0400

    update

--- test_git.py
+++ test_git.py
@@ -1,15 +1,26 @@
 SITES = [
-   "https://test1.com",
-   "https://test2.com",
+   "https://test1.com"
+   ""https://test1.com""
    "https://test3.com",
    "https://test4.com",
    "https://test5.com",
-   "Fatima Ali",
    "https://test6.com",
-   "Fatima Ali",
-   "Fatima Ali",
-   "Fatima Ali",
-   "Fatima Ali",   
-   "Fatima Ali",
-   "https://test7.com"
+   "https://test7.com",
+   SITES = [
+   "https://test1.com"
+   ""https://test1.com""
+   "https://test3.com",
+   "https://test4.com",
+   "https://test5.com",
+   "https://test6.com",
+   "https://test7.com",
+   "https://test8.com",
+   "https://test9.com",
+   "https://test10.com"
 ]
+
+   "https://test8.com",
+   "https://test9.com",
+   "https://test10.com"
+]
+
